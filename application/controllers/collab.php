8b38b3be3267fda09a919508baa94e3c83335034
Iordache Claudiu <claudiu.iordache@hotmail.com>
2013-06-21 14:35:23 +0300

    Alfa 0.4

--- application/controllers/collab.php
+++ application/controllers/collab.php
@@ -5,11 +5,9 @@ class Collab extends CI_Controller {
 		$this -> is_loggedin();
 		$this -> load -> model('chat_model');
 		$this -> load -> model('document_model');
-		$this -> view_data['chat_id'] = 1;
-		$this -> view_data['user_id'] = $this -> getUserID();
-		$this -> session -> set_userdata('last_chat_message_id_' . $this -> view_data['chat_id'], 0);
+		
 	}
-
+	
 	function is_loggedin() {
 		$log_status = $this -> session -> userdata('is_logged_in');
 		if (!isset($log_status) || $log_status != TRUE) {
@@ -18,12 +16,18 @@ class Collab extends CI_Controller {
 	}
 
 	function createNewDocument() {
+		$this -> view_data['chat_id'] = 1;
+		$this -> view_data['user_id'] = $this -> getUserID();
+		$this -> session -> set_userdata('last_chat_message_id_' . $this -> view_data['chat_id'], 0);
 		$this -> view_data['main_content_collab'] = 'load_form';
 		$this -> view_data['page_content'] = 'view_chat';
 		$this -> load -> view('includes/template_collab', $this -> view_data);
 	}
 
 	function noLoad() {
+		$this -> view_data['chat_id'] = 1;
+		$this -> view_data['user_id'] = $this -> getUserID();
+		$this -> session -> set_userdata('last_chat_message_id_' . $this -> view_data['chat_id'], 0);
 		$this -> view_data['main_content_collab'] = 'no_load_form';
 		$this -> view_data['page_content'] = 'view_chat';
 		$this -> load -> view('includes/template_collab', $this -> view_data);
@@ -92,24 +96,24 @@ class Collab extends CI_Controller {
 
 	function _get_chat_messages($chat_id) {
 		$last_chat_message_id = (int)$this -> session -> userdata('last_chat_message_id_' . $chat_id);
-
 		$chat_messages = $this -> chat_model -> get_chat_message($chat_id, $last_chat_message_id);
 		if ($chat_messages -> num_rows() > 0) {
 
-			$last_chat_message_id = $chat_messages -> row($chat_messages -> num_rows() - 1) -> chat_message_id;
+			$last_chat_message_id = $chat_messages -> row($chat_messages -> num_rows()-1) -> chat_message_id;
 			$this -> session -> set_userdata('last_chat_message_id_' . $chat_id, $last_chat_message_id);
-
 			$chat_messages_html = '<ul>';
 			foreach ($chat_messages->result() as $chat_message) {
 				$query_str = "SELECT * FROM users where username=?";
 				$result = $this -> db -> query($query_str, $this -> session -> userdata('username'));
 				if ($result -> num_rows() == 1) {
+					
 					foreach ($result->result() as $user) {
 						$li_class = ($user -> user_id == $chat_message -> user_id) ? 'class="by_current_user"' : '';
 					}
 				}
 
-				$chat_messages_html .= '<li ' . $li_class . '><span class="chat_message_header">' . $chat_message -> chat_message_timestamp . ' by ' . $chat_message -> username . '</span><p class="message_content">' . $chat_message -> chat_message_content . '</p></li>';
+				$chat_messages_html .= '<li ' . $li_class . '><span class="chat_message_header">' . $chat_message -> chat_message_timestamp . ' by ' . $chat_message -> username . '</span><p class="message_content">' . $chat_message -> chat_message_content . 
+			'</p></li>';
 			}
 
 			$chat_messages_html .= '</ul>';
@@ -119,7 +123,7 @@ class Collab extends CI_Controller {
 			exit();
 		} else {
 
-			$result = array('status' => 'ok', 'content' => $chat_messages_html);
+			$result = array('status' => 'ok', 'content' => '');
 			return json_encode($result);
 			exit();
 		}
